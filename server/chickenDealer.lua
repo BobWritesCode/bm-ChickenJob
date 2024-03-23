db0a22542589a041a3368e8bdaa6dd28a82f91ef
BobWritesCode <warwick_hart@hotmail.com>
2024-03-23 21:48:34 +0000

    change capitalisation of vars

--- server/chickenDealer.lua
+++ server/chickenDealer.lua
@@ -1,40 +1,40 @@
-local DealerPed = nil
-local DealerNetID = nil
+local dealerPed = nil
+local dealerNetID = nil
 
 RegisterServerEvent('bm-chickenjob:SpawnDealerPed', function()
   DebugPrint('bm-chickenjob:SetDealerPed')
   local model = Config.Locations.chickenDealer.PedModel
   local coords = Config.Locations.chickenDealer.coords
   local h = Config.Locations.chickenDealer.PedModelHeading
-  DealerPed = CreatePed(0, model, coords, h, true, false)
-  DealerNetID = NetworkGetNetworkIdFromEntity(DealerPed)
-  TriggerClientEvent('bm-chickenjob:AssignNewChickenDealerEnt', -1 , DealerNetID)
+  dealerPed = CreatePed(0, model, coords, h, true, false)
+  dealerNetID = NetworkGetNetworkIdFromEntity(dealerPed)
+  TriggerClientEvent('bm-chickenjob:AssignNewChickenDealerEnt', -1 , dealerNetID)
 end)
 
 QBCore.Functions.CreateCallback('bm-chickenjob:GetDealerPed',
   function(_, cb)
     DebugPrint2('bm-chickenjob:GetDealerPed: ', dealerNetID)
-    cb(DealerNetID)
+    cb(dealerNetID)
   end)
 
-  CreateThread(function()
-    while true do
-      while not DealerPed do
+CreateThread(function()
+  while true do
+    while not dealerPed do
+      TriggerEvent('bm-chickenjob:SpawnDealerPed')
+      Wait(100)
+    end
+    Wait(5000)
+    if dealerPed then
+      local c  = GetEntityCoords(dealerPed)
+      local distdiff = #(c - Config.Locations.chickenDealer.coords)
+      if distdiff> 5 then
+        DeleteEntity(dealerPed)
         TriggerEvent('bm-chickenjob:SpawnDealerPed')
-        Wait(100)
       end
-      Wait(5000)
-      if DealerPed then
-        local c  = GetEntityCoords(DealerPed)
-        local distdiff = #(c - Config.Locations.chickenDealer.coords)
-        if distdiff> 5 then
-          DeleteEntity(DealerPed)
-          TriggerEvent('bm-chickenjob:SpawnDealerPed')
-        end
-        if GetPedSourceOfDeath(DealerPed) ~= 0 then
-          DeleteEntity(DealerPed)
-          TriggerEvent('bm-chickenjob:SpawnDealerPed')
-        end
-    end
-    end
-  end)
+      if GetPedSourceOfDeath(dealerPed) ~= 0 then
+        DeleteEntity(dealerPed)
+        TriggerEvent('bm-chickenjob:SpawnDealerPed')
+      end
+  end
+  end
+end)
